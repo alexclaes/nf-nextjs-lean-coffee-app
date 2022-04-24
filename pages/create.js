aa798e94d557ae0f154ec720543cb736feeb91c6
Alexander Claes <hello@aclaes.com>
2022-04-24 12:27:17 +0200

    Refactor CardCreateForm

--- pages/create.js
+++ pages/create.js
@@ -1,78 +1,10 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { useSession } from "next-auth/react";
-import { Button, Container, Grid, TextField, Typography } from "@mui/material";
+import { Container } from "@mui/material";
+import CardCreateForm from "../src/components/CardCreateForm";
 
 export default function Create() {
-  const { data: session } = useSession();
-
-  const [nameValue, setNameValue] = useState("");
-  const [contentValue, setContentValue] = useState("");
-  const [nameInputDisabled, setNameInputDisabled] = useState(false);
-
-  const router = useRouter();
-
-  useEffect(() => {
-    if (session && !nameValue) {
-      setNameValue(session.user?.name);
-      setNameInputDisabled(true);
-    }
-  }, [session, nameValue]);
-
-  async function onFormSubmit(event) {
-    event.preventDefault();
-
-    const response = await fetch("/api/card/create", {
-      method: "POST",
-      body: JSON.stringify({
-        name: nameValue,
-        content: contentValue,
-      }),
-    });
-    console.log(await response.json());
-
-    router.push("/cards");
-  }
-
   return (
     <Container maxWidth="xl">
-      <form onSubmit={onFormSubmit}>
-        <Grid container spacing={4}>
-          <Grid item xs={12}>
-            <Typography variant="h1">Add a new card</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              name="content"
-              label="Content"
-              fullWidth
-              multiline
-              rows={4}
-              value={contentValue}
-              onChange={(event) => {
-                setContentValue(event.target.value);
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              name="name"
-              label="Name"
-              fullWidth
-              value={nameValue}
-              onChange={(event) => {
-                setNameValue(event.target.value);
-              }}
-              disabled={nameInputDisabled}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button variant="contained" type="submit">
-              Create
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
+      <CardCreateForm />
     </Container>
   );
 }
---
+++ src/components/CardCreateForm.js
@@ -0,0 +1,76 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { Button, Grid, TextField, Typography } from "@mui/material";
+
+export default function CardCreateForm() {
+  const { data: session } = useSession();
+
+  const [nameValue, setNameValue] = useState("");
+  const [contentValue, setContentValue] = useState("");
+  const [nameInputDisabled, setNameInputDisabled] = useState(false);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session && !nameValue) {
+      setNameValue(session.user?.name);
+      setNameInputDisabled(true);
+    }
+  }, [session, nameValue]);
+
+  async function onFormSubmit(event) {
+    event.preventDefault();
+
+    const response = await fetch("/api/card/create", {
+      method: "POST",
+      body: JSON.stringify({
+        name: nameValue,
+        content: contentValue,
+      }),
+    });
+    console.log(await response.json());
+
+    router.push("/cards");
+  }
+
+  return (
+    <form onSubmit={onFormSubmit}>
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <Typography variant="h1">Add a new card</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            name="content"
+            label="Content"
+            fullWidth
+            multiline
+            rows={4}
+            value={contentValue}
+            onChange={(event) => {
+              setContentValue(event.target.value);
+            }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            name="name"
+            label="Name"
+            fullWidth
+            value={nameValue}
+            onChange={(event) => {
+              setNameValue(event.target.value);
+            }}
+            disabled={nameInputDisabled}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="contained" type="submit">
+            Create
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
+  );
+}
